8c0ecc3539c78195df68c8a76f17ce88603b09e8
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2019-04-07 15:53:01 +0300

    base template

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>Hello ReactWarriors</div>;
+};
+
+export default App;
---
+++ src/templates.js
@@ -0,0 +1,31 @@
+// MovieItem
+// <div className="card" style={{ width: "100%" }}>
+//   <img
+//     className="card-img-top"
+//     src={`https://image.tmdb.org/t/p/w500${item.backdrop_path ||
+//       item.poster_path}`}
+//     alt=""
+//   />
+//   <div className="card-body">
+//     <h6 className="card-title">{item.title}</h6>
+//     <div className="d-flex justify-content-between align-items-center">
+//       <p className="mb-0">Rating: {item.vote_average}</p>
+//       <button
+//         type="button"
+//         className="btn btn-success"
+//         onClick={this.changeWillWatch}
+//       >
+//         Will Watch
+//       </button>
+//     </div>
+//   </div>
+// </div>;
+// MovieListWillWatch
+// <h4>Will Watch: movies</h4>
+// <ul className="list-group">
+//     <li className="list-group-item">
+//       <div className="d-flex justify-content-between">
+//       </div>
+//     </li>
+//   ))}
+// </ul>
