cf77653eeec62ac33fdeded1ae3cb3f37e1f70be
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2019-06-05 18:57:30 +0300

    5.06

--- src/templates.js
+++ src/templates.js
@@ -3,13 +3,13 @@
 //   <img
 //     className="card-img-top"
 //     src={`https://image.tmdb.org/t/p/w500${item.backdrop_path ||
-//       item.poster_path}`}
+//       data.poster_path}`}
 //     alt=""
 //   />
 //   <div className="card-body">
-//     <h6 className="card-title">{item.title}</h6>
+//     <h6 className="card-title">{data.title}</h6>
 //     <div className="d-flex justify-content-between align-items-center">
-//       <p className="mb-0">Rating: {item.vote_average}</p>
+//       <p className="mb-0">Rating: {data.vote_average}</p>
 //       <button
 //         type="button"
 //         className="btn btn-secondary"
